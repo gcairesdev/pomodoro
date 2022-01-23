3daaa6b0d0b79c69db8d74808967ce9f33f435fb
Guilherme Caires <pessoalgcaires@gmail.com>
2022-01-23 18:50:42 -0300

    refactor: move tabs to tab.js

--- js/tab.js
+++ js/tab.js
@@ -1,3 +1,9 @@
+const tabs = [
+    ['Pomodoro', $('#pomodoroTab'), '#d95550', 25],
+    ['Short Break', $('#shortBreakTab'), '#5092d9', 5],
+    ['Long Break', $('#longBreakTab'), '#375192', 15],
+];
+
 const clearActiveTab = () => $('.active').classList.remove('active');
 
 const getActiveTab = () => $('.active').innerText;
@@ -6,3 +12,22 @@ const setActiveTab = tab => {
     clearActiveTab();
     tab.classList.add('active');
 }
+
+const onClickTabs = () => {
+    tabs.forEach(([name, tab, color, time]) => {
+        tab.onclick = () => {
+            timerIsRunning() && stopTimer();
+
+            setActiveTab(tab);
+            setButtonColor(color);
+            setPageColor(color);
+            setPageTitle(name);
+
+            timeInMinutes = time;
+            timerInSeconds = minutesToSeconds(timeInMinutes);
+            updateDisplayTimer(formatTimer(timerInSeconds));
+        };
+    });
+}
+
+onClickTabs();
